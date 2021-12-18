6a67a77b03f3e6045889a2529e4f4f0335b9938d
Matan.Socher <matan.socher@zoominfo.com>
2021-12-18 11:39:08 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App.jsx';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import authUserDetailsReducer from './redux/authUserDetails';
+import currentChatIdReducer from './redux/currentChatId';
+import currentChatUserReducer from './redux/currentChatUser';
+import currentReplyMessageReducer from './redux/currentReplyMessage';
+import starredMessagesIdsReducer from './redux/starredMessagesIds';
+
+const store = configureStore({
+  reducer: {
+    authUserDetails: authUserDetailsReducer,
+    currentChatId: currentChatIdReducer,
+    currentChatUser: currentChatUserReducer,
+    currentReplyMessage: currentReplyMessageReducer,
+    starredMessagesIds: starredMessagesIdsReducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false })
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
